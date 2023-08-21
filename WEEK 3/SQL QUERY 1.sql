94edfe175c8889c45b62447ff33ce0e192851bbc
Babi <babisunu@gmail.com>
2023-08-21 16:42:41 +0200

    SQL QUERY1

---
+++ WEEK 3/SQL QUERY 1.sql
@@ -0,0 +1,54 @@
+USE sakila ;
+SHOW TABLES;
+
+SELECT * FROM  sakila.actor;
+SELECT * FROM  sakila.film;
+SELECT * FROM  sakila.customer;
+
+SELECT title 
+FROM sakila.film ;
+
+  # SELECT * FROM SAKILA.LANGUAGE
+ 
+ SELECT name AS LANGUAGE
+ FROM SAKILA.LANGUAGE ;
+ 
+ 
+#  SELECT  * FROM SAKILA.STAFF
+ SELECT first_name 
+ FROM sakila.staff ;
+ 
+ SELECT DISTINCT (release_year)
+ FROM FILM ;
+ 
+  # SELECT * FROM  STORE
+  
+  SELECT COUNT(store_id) AS NO_OF_STORE
+  FROM store ;
+  
+  SELECT COUNT(manager_staff_id) AS NO_OF_EMPLOYEES
+  FROM store ;
+  
+  
+  SELECT DISTINCT (last_name )
+  FROM ACTOR ;
+  
+  
+ # SELECT *  FROM FILM
+ SELECT title 
+ FROM FILM
+ limit 10;
+ 
+ SELECT first_name
+ FROM ACTOR 
+ WHERE first_name=  "SCARLETT";
+ 
+
+ SELECT  * FROM FILM
+ WHERE title LIKE '%ARMAGEDDON%' AND  
+length> 100;
+
+SELECT * FROM film
+WHERE special_features LIKE '%Behind the Scenes%';
+
+#SELECT * FROM FILM;
