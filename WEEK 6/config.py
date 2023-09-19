4294b55fcdc340c622a0b17d1ed6c4f4ae0dc557
Babi <babisunu@gmail.com>
2023-09-19 15:05:36 +0200

    lab

---
+++ WEEK 6/config.py
@@ -0,0 +1,11 @@
+#client_id ="61d37ae625464bd9b635464c00b38fcc"
+#client_secret = "5d41495f2bd9457394939f2e05c332f8"
+
+#client_id ="835596ade2e44f6eb0e3dc8e66a29b2a"
+#client_secret = "814bc4085df1478fb4a43d3b6f9eb475"
+
+#client_id = "732a6e8804c94c2d91200c54dc37e796"
+#client_secret = "bb44038e58aa4ec8b417816ff182e807"
+
+client_id = "ee0946bba73b427a917a53ec9502210e"
+client_secret = "e545f220f1c64bf398c979a7cf6900bb"
